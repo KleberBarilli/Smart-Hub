4a010a4fab8d6c8a4db0c4a8c09db4a7deed2604
kleber.barilli73@gmail.com <kleber.barilli73@gmail.com>
2024-05-18 16:41:31 -0300

    wip user data

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,34 @@ import { redirect } from "next/navigation";
 import { DashboardNav } from "../components/dashboard-nav";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
+async function getUserData({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}) {
+  //fetch to api
+
+  const user = {
+    id,
+    firstName,
+    lastName,
+    email,
+    profileImage,
+  };
+  if (!user) {
+    //crteate user in api
+  }
+  return user;
+}
+
 export default async function DashboardLayout({
   children,
 }: {
@@ -13,6 +41,13 @@ export default async function DashboardLayout({
   if (!user) {
     return redirect("/");
   }
+  await getUserData({
+    email: user.email as string,
+    firstName: user.given_name as string,
+    id: user.id as string,
+    lastName: user.family_name as string,
+    profileImage: user.picture,
+  });
   return (
     <div className="flex flex-col space-y-6 mt-10">
       <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
