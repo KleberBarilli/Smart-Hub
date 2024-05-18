f84c10f9fee806cfd999711ee1f042c25eea5538
kleber.barilli73@gmail.com <kleber.barilli73@gmail.com>
2024-05-18 15:45:30 -0300

    feat: dashboard

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,22 +7,28 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { UserNav } from "./user-nav";
 
 export async function Navbar() {
-  const { isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = await getUser();
 
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <h1 className="font-bold text-3xl">Smart Hub</h1>
+          <h1 className="font-bold text-3xl">
+            <span className="text-primary">Smart Hub</span>
+          </h1>
         </Link>
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
           {(await isAuthenticated()) ? (
-            <LogoutLink>
-              <Button>Logout</Button>
-            </LogoutLink>
+            <UserNav
+              email={user?.email as string}
+              image={user?.picture as string}
+              name={user?.given_name as string}
+            />
           ) : (
             <div className="flex items-center gap-x-5">
               <LoginLink>
