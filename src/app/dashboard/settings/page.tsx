3094281d11ae0300296e0a82cea9a570b90531d7
kleber.barilli73@gmail.com <kleber.barilli73@gmail.com>
2024-05-21 19:28:27 -0300

    feat: add settings labels

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,110 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import { SubmitButton } from "@/app/components/submit-button";
+import { revalidatePath, unstable_noStore as noStore } from "next/cache";
+
+async function getData(userId: string) {
+  noStore();
+  //find user in api
+  const data = {
+    name: "John doe mocked",
+    email: "email@mock.com",
+    telegramName: "@john_doe",
+  };
+
+  return data;
+}
+
+export default async function SettingPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
+
+  async function postData(formData: FormData) {
+    "use server";
+
+    const name = formData.get("name") as string;
+
+    const telegramName = formData.get("telegram_name") as string;
+
+    //update user in api
+    const data = {
+      name: `${name} updated`,
+      telegramName,
+    };
+
+    revalidatePath("/", "layout");
+  }
+
+  return (
+    <div className="grid items-start gap-8">
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl md:text-4xl">Settings</h1>
+          <p className="text-lg text-muted-foreground">Your Profile settings</p>
+        </div>
+      </div>
+
+      <Card>
+        <form action={postData}>
+          <CardHeader>
+            <CardTitle>General Data</CardTitle>
+            <CardDescription>
+              Please provide general information about yourself. Please dont
+              forget to save
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="space-y-2">
+              <div className="space-y-1">
+                <Label>Your Name</Label>
+                <Input
+                  name="name"
+                  type="text"
+                  id="name"
+                  placeholder="Your Name"
+                  defaultValue={data?.name ?? undefined}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label>Your Email</Label>
+                <Input
+                  name="email"
+                  type="email"
+                  id="email"
+                  placeholder="Your Email"
+                  disabled
+                  defaultValue={data?.email as string}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label>Telegram User name</Label>
+                <Input
+                  name="telegram_name"
+                  type="text"
+                  id="telegram_name"
+                  placeholder="Your Name"
+                  defaultValue={data?.telegramName ?? undefined}
+                />
+              </div>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <SubmitButton />
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
+}
